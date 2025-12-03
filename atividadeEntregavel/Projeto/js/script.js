72a20e342536578a1fd4464ef7a93d7484e1a069
I'VéX <pedro.pericles09@aluno.ifce.edu.br>
2025-12-03 10:47:50 -0300

    Delete atividadeEntregavel/Projeto directory

--- atividadeEntregavel/Projeto/js/script.js
+++
@@ -1,4 +0,0 @@
-const toggle = document.getElementById("themeToggle");
-toggle.addEventListener("click",()=>{document.body.classList.toggle("dark");})
-
-function scrollToSection(id){document.getById(id).scrollView({behavior:"smooth"});}
